f618db40363e2905cba2750712be154bc71aa191
Swift <swift@wozmac3.local>
2018-01-15 21:42:25 -0200

    pushing challenge to branch.

--- DesafioFork/ViewController.swift
+++ DesafioFork/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage(_ sender: Any) {
+        let greeting = showGreeting()
+        Alert.showAlert(viewController: self, title: greeting.rawValue)
+    }
+    
+    func showGreeting() -> GreetingType {
+        let current = Date()
+        guard let midDay = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: current) else {
+            return .none
+        }
+        guard let sixOClock = Calendar.current.date(bySettingHour: 18, minute: 0, second: 0, of: current) else {
+            return .none
+        }
+        if current >= midDay && current <= sixOClock {
+            return .afternoon
+        } else if current >= sixOClock {
+            return .night
+        } else {
+            return .day
+        }
+    }
+    
 }
+
+enum GreetingType: String {
+    case day = "Bom dia"
+    case afternoon = "Boa tarde"
+    case night = "Boa noite"
+    case none = ""
+}
+
+class Alert {
+    
+    class func showAlert(viewController: UIViewController,title: String? = "", message: String? = "") {
+        let alert = UIAlertController.init(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        viewController.present(alert, animated: true, completion: nil)
+    }
+}
+
 
